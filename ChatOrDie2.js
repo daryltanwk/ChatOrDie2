3fd1b3ab6fb75085c0d318ae9c6d95faa07d211b
Daryl Tan <daryltanwk@gmail.com>
2015-07-24 11:33:23 +0800

    Main Module ===
    - added route configuration
    - cleaned up main js
    - added loading template (WIP)
    - cleaned/modified login panel

--- ChatOrDie2.js
+++ ChatOrDie2.js
@@ -1,23 +1,8 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Template.loginPanel.events({
+        'click #loginToRegister': function(evt, template) {
+            evt.preventDefault();
+            Router.go('/register');
+        }
+    });
 }
--- MainRouteController.js
+++ MainRouteController.js
@@ -1,5 +1,21 @@
 MainController = RouteController.extend({});
 
-Router.route('/', function() {
-	this.render('loginPanel');
+Router.route('/', {
+    template: 'loginPanel',
+    name: 'root'
+});
+
+Router.onBeforeAction(function() {
+    if (Meteor.loggingIn()) {
+        //Render the 'loadingPage' template
+        this.render('loadingPage');
+    } else if (Meteor.user()) {
+        //Carry on, nothing to do here.
+        this.next();
+    } else {
+    	//Sorry no access, go back to your roots!
+        Router.go('/');
+    }
+}, {
+    except: ['register', 'root']
 });
